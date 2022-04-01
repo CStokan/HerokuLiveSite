bfa1c314197df259ae4c79c00f365ce270e9e1d7
CStokan <81194869+CStokan@users.noreply.github.com>
2022-04-01 10:09:45 -0400

    Added to heroku livesite

---
+++ Config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SessionSecret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://127.0.0.1/contacts";
+exports.RemoteURI = "mongodb+srv://stokanc:wD0qSmAgy1Kz44wn@cluster0.1f170.mongodb.net/contacts?retryWrites=true&w=majority";
+exports.HostName = "MongoDB Atlas";
+exports.SessionSecret = "WEBD6201SessionSecret";
+//# sourceMappingURL=db.js.map
